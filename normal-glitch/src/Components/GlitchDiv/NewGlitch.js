8662883a4f502aa837a2578d4fad99b5b42df910
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-11-17 22:33:26 -0600

    added sample text

--- normal-glitch/src/Components/GlitchDiv/NewGlitch.js
+++ normal-glitch/src/Components/GlitchDiv/NewGlitch.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
 import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass.js'
 import {GlitchPass} from 'three/examples/jsm/postprocessing/GlitchPass.js'
+import fontJSON from './fontJSON.json'
 export default function NewGlitch() {
     const GlitchRef = useRef()
     useEffect(() => {
@@ -22,22 +23,30 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     //
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera = new THREE.PerspectiveCamera( 150, window.innerWidth / window.innerHeight, 1, 1000 );
     camera.position.z = 400;
     scene = new THREE.Scene();
     scene.fog = new THREE.Fog( 0x000000, 1, 1000 );
     object = new THREE.Object3D();
     scene.add( object );
-    var geometry = new THREE.BoxBufferGeometry( 1, 2, 2 );
-    for ( var i = 0; i < 100; i ++ ) {
-        var material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random(), flatShading: true } );
-        var mesh = new THREE.Mesh( geometry, material );
-        mesh.position.set( Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5 ).normalize();
-        mesh.position.multiplyScalar( Math.random() * 400 );
-        mesh.rotation.set( Math.random() * 2, Math.random() * 2, Math.random() * 2 );
-        mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 50;
-        object.add( mesh );
-    }
+    // var geometry = new THREE.SphereBufferGeometry( 1, 1, 1);
+    // for ( var i = 0; i < 100; i ++ ) {
+    //     var material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random(), flatShading: true } );
+    //     var mesh = new THREE.Mesh( geometry, material );
+    //     mesh.position.set( Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5 ).normalize();
+    //     mesh.position.multiplyScalar( Math.random() * 400 );
+    //     mesh.rotation.set( Math.random() * 2, Math.random() * 2, Math.random() * 2 );
+    //     mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 50;
+    //     object.add( mesh );
+    // }
+    //FOnt
+
+    var loader = new THREE.FontLoader();
+    var font =loader.parse(fontJSON)
+    var geometry = new THREE.TextGeometry("Test String",{font:font, size: 160, height: 10, material: 0, bevelThickness: 1, extrudeMaterial:10})
+    var material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
+    var mesh = new THREE.Mesh(geometry,material)
+    object.add(mesh)
     scene.add( new THREE.AmbientLight( 0x222222 ) );
     light = new THREE.DirectionalLight( 0xffffff );
     light.position.set( 1, 1, 1 );
@@ -61,8 +70,8 @@ function onWindowResize() {
 }
 function animate() {
     requestAnimationFrame( animate );
-    object.rotation.x += 0.005;
-    object.rotation.y += 0.01;
+    // object.rotation.x += 0.005;
+    // object.rotation.y += 0.01;
     composer.render();
 }
     }, [GlitchRef])
